53d0e7b26f4c4a51e8ec7ba28313eb281c6708c2
Gabe Nuñez <gabe@gabenunez.com>
2019-04-04 11:03:04 -0700

    Initial commit!

---
+++ spellbook/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a start...</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
